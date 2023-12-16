3b9f0bf66fed4ad61992cb33f4a35868582d1d8e
hansa gennoo <hansagennoo10@gmail.com>
2023-12-16 15:26:38 +0400

    Rails Task Manager

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  get 'tasks', to: 'tasks#index'
+
+  get 'tasks/new', to: 'tasks#new'
+  get 'tasks/:id', to: 'tasks#show', as: :toto
+
+  post 'tasks', to: 'tasks#create'
+
+  get "tasks/:id/edit", to: "restaurants#edit"
 end
